0a9737ac77f7fcf7067cc7c09d64198de24f7f76
AngelyThC <angelyth321@umes.edu.gt>
2023-11-03 15:50:01 -0600

    hecho ouyea

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+class Cliente(BaseModel):
+    id:int
+    nombre:str
+    telefono:str
+    direccion:str
+    frecuente:str
+    puntosAcumulados:int
+    
+    class Config:
+        orm_mode=True
+
+class ClienteUpdate(BaseModel):   
+    nombre:str
+   
+
+    class Config:
+        orm_mode =True
+
+class Respuesta(BaseModel):   
+    mensaje:str
+   
